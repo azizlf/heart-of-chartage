3f39f394a93275908490b64aae95927668746b81
Louifi Mohamed aziz <113394781+azizlf@users.noreply.github.com>
2023-02-18 00:44:52 +0100

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDFSZvYLHJgaUnhhMfprqB3lk9ZPWBm5P0",
+    authDomain: "ihebfront.firebaseapp.com",
+    projectId: "ihebfront",
+    storageBucket: "ihebfront.appspot.com",
+    messagingSenderId: "215501404523",
+    appId: "1:215501404523:web:663b034a8fdf455daef42e"
+},
+baseURL :'https://heart-of-carthage-dubai.com/backend'
+};
