14d0d8292babbdbe9b1cf4a52596dbb550d2ac46
azizlf <developer.mohamedazizlouifi@gmail.com>
2023-02-18 13:01:30 +0100

    change design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,71 @@ export class AppComponent {
   title = 'Heart Of Carthage Real Estate Dubai';
   message:any
   public phone = "+971 58 218 9263"
- public messg = "ارسل الآن طلبك";
+  public messg = "ارسل الآن طلبك";
   constructor(private  messagingService:MessagingService ,private http:HttpClient,private meta:Meta){
 
   }
+  
+  elementSurvey:any
+  phoneScreen:any
+  
+  animationEleSurvey(){
+
+    this.elementSurvey = document.getElementById("surveyContainer")
+    this.elementSurvey.style.display="block"
+
+    setTimeout(()=>{
+      this.elementSurvey = document.getElementById("logoSurvey")
+      this.elementSurvey.style.transform="scale(1)"
+      this.elementSurvey.style.top="5%"
+      this.elementSurvey.style.left="2%"
+    },1200)
+
+    setTimeout(()=>{
+      this.elementSurvey = document.getElementById("nameLogo")
+      this.elementSurvey.style.display="block"
+
+    },2600)
+
+    setTimeout(()=>{
+      this.elementSurvey = document.getElementById("questionSurvey")
+      this.elementSurvey.style.display="block"
+
+    },3600)
+
+    setTimeout(()=>{
+      this.elementSurvey = document.getElementById("btnYes")
+      this.elementSurvey.style.display="block"
+      this.elementSurvey = document.getElementById("btnNo")
+      this.elementSurvey.style.display="block"
+
+    },4000)
+
+    setTimeout(()=>{
+      this.elementSurvey = document.getElementById("skipBtn")
+      this.elementSurvey.style.opacity="1"
+      this.elementSurvey.style.pointerEvents="all"
+
+    },5600)
+
+
+
+  }
+
+  surveyIsOpen = false
+
   ngOnInit() {
+
+    this.phoneScreen = window.matchMedia('(max-width: 700px)')
+
+    if(this.phoneScreen.matches){  
+      setTimeout(()=>{
+        this.animationEleSurvey()
+        this.surveyIsOpen = true   
+      },5000)
+
+    }
+
     this.meta.addTags([
       {
         name: 'keywords',
@@ -30,6 +90,25 @@ export class AppComponent {
     this.messagingService.requestPermission()
     this.messagingService.receiveMessage()
     this.message = this.messagingService.currentMessage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   //   const headers = new HttpHeaders({'Content-Type':'application/json','Authorization':"czEAEx7YWFnCC8Ei3wBBdv:APA91bEOyuQ0J4h2oD_OM2n292PsNI4bZX4pe1C2wTpg0ExGiecP3j"});
   //  var noti=  {notification: {
   //   title: "Hey there",
